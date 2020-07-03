510664dfe1313529db5382367701704417f2ba1e
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-03 14:15:41 +0200

    On tweet button, tweet a quote

--- index.js
+++ index.js
@@ -13,6 +13,14 @@
 let text = document.getElementById("text");
 let author = document.getElementById("author");
 let newQuoteButton = document.getElementById("new-quote");
+let tweetButton = document.getElementById("tweet-quote");
+
+tweetButton.addEventListener("click", tweetQuote = (event) => {
+    //Change the value of href attributes in a element
+    //We add query parameters to tweet the quote
+    tweetButton.href = tweetButton.href + "?text=this%20is%20a%20tweet";
+    console.log(tweetButton);
+});
 
 //Add a handler on click event
 newQuoteButton.addEventListener("click", clickButton = (event) => {
