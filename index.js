cbdb9dca6aa9a6ff69f787fd7a2f89599a38e226
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-03 13:51:09 +0200

    init project and first commmit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Random Quote Machine');
+
+//First load => random quote in #text
+
+//First load => author in #author
+
+//#new-quote button is clicked => new quote in #text
+
+//#tweet-quote clicked => "twitter.com/intent/tweet" + attribute to tweet 
+
+//#quote-box => horizontaly centred + run tests with browser's zoom level at 100% and page maximized
+
